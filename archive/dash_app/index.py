7e67cbe9effca0387d37a0967c0cd5a9934cde0a
cheekean <cheekeanchan@CHANs-MacBook-Pro.local>
2023-06-01 16:57:57 +0800

    added owl-vit

--- archive/dash_app/index.py
+++
@@ -1,7 +0,0 @@
-from app import app
-from components.templates.main import make_layout
-
-app.layout = make_layout()
-
-if __name__ == '__main__':
-    app.run_server(debug=False)
--- validation_app/index.py
+++
@@ -1,6 +0,0 @@
-from app import app
-from components.templates.main import make_layout
-app.layout = make_layout()
-
-if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8050, debug=False)
