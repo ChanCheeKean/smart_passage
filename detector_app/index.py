c9dd136470266c7a7b3101a414659ec477bc14c4
cheekean <cheekeanchan@CHANs-MacBook-Pro.local>
2023-06-07 13:25:42 +0800

    updated

--- detector_app/index.py
+++ detector_app/index.py
@@ -3,4 +3,4 @@ from components.pg1 import layout
 app.layout = layout
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8050,debug=False)
+    app.run_server(host='0.0.0.0', port=5000, debug=False)
