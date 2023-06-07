50e44dbaed1e702c28d6519b2d5a970824999ffd
cheekean <cheekeanchan@CHANs-MacBook-Pro.local>
2023-06-07 13:19:09 +0800

    updated

--- detector_app/index.py
+++ detector_app/index.py
@@ -6,4 +6,4 @@ from dash import html, dcc
 app.layout = html.Div()
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8050,debug=False)
+    app.run_server(host='0.0.0.0', port=5000,debug=False)
