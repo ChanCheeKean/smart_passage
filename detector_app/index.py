4e50b14d523023409d20e344699ce0ea2cdb649c
cheekean <cheekeanchan@CHANs-MacBook-Pro.local>
2023-06-07 13:16:13 +0800

    updated

--- detector_app/index.py
+++ detector_app/index.py
@@ -1,6 +1,9 @@
 from app import app
 from components.pg1 import layout
-app.layout = layout
+
+from dash import html, dcc
+
+app.layout = html.Div()
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=8050,debug=False)
