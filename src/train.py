bbdb60f78d218e9e5d1b22e49bf7e5f3417a34c1
cheekean <cheekeanchan@CHANs-MacBook-Pro.local>
2023-06-09 14:35:06 +0800

    update train script

--- src/train.py
+++ src/train.py
@@ -1,11 +1,13 @@
 from ultralytics import YOLO
 
 # Load a model, # build from YAML and transfer weights
-model = YOLO('yolov8n.yaml').load('yolov8n.pt')  
+model = YOLO('yolov8n.pt') 
 
 # Train the model
 model.train(
-    data='./config/data.yaml', 
+    data="./config/data.yaml", 
     epochs=2, 
-    imgsz=640
+    imgsz=640,
+    batch=64,
+    device=None
 )
