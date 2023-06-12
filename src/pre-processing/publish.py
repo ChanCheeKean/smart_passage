56d9d94701d83b55175aeaa6b53ff4d4da44483e
cheekean <cheekeanchan@CHANs-MacBook-Pro.local>
2023-06-12 18:17:25 +0800

    added publish

--- src/pre-processing/publish.py
+++ src/pre-processing/publish.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import argparse
 from io import BytesIO
 import pandas as pd
@@ -9,12 +10,27 @@ MINIO_ACCESS_KEY = os.environ.get('MINIO_ACCESS_KEY')
 MINIO_SECRET_KEY = os.environ.get('MINIO_SECRET_KEY')
 domain = '192.168.96.95:9000'
 
+def upload_local_directory_to_minio(client, local_path, bucket_name, minio_path):
+    # assert os.path.isdir(local_path)
+    for local_file in glob.glob(local_path + '/**'):
+        local_file = local_file.replace(os.sep, "/")
+
+        if not os.path.isfile(local_file):
+            upload_local_directory_to_minio(
+                client, local_file, bucket_name, minio_path + "/" + os.path.basename(local_file))
+        else:
+            remote_path = os.path.join(
+                opt.prefix, local_file[1 + len(opt.origin):])
+            remote_path = remote_path.replace(os.sep, "/") 
+            client.fput_object(opt.bucket, remote_path, local_file)
+
 def main(opt):
     minioClient = Minio(
         domain, 
         access_key=MINIO_ACCESS_KEY,
         secret_key=MINIO_SECRET_KEY,
         secure=False)
+    upload_local_directory_to_minio(minioClient, opt.origin, opt.bucket, opt.prefix)
 
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
@@ -27,4 +43,4 @@ if __name__ == '__main__':
     opt = parse_opt()
     main(opt)
 
-# python publish.py --origin "../data/landing" --bucket "test" --prefix "landing"
+# python publish.py --origin "../data/landing" --bucket "test" --prefix "/data/landing"
